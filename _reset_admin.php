8e82c532277fcf315747cea8ca5e0c4739fae827
linhanh2005 <23070519@vnu.edu.vn>
2026-05-15 13:44:56 +0700

    Update _reset_admin.php

--- _reset_admin.php
+++ _reset_admin.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 1);t
 error_reporting(E_ALL);
 
 echo "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Reset Admin</title>";
