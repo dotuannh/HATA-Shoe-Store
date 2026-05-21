4600def20a3ef8ff78e4fa3b72d2357aa5089951
LHTrang <23070828@vnu.edu.vn>
2026-05-21 10:09:20 +0700

    Implement logout functionality in logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,15 @@
+<?php
+require_once 'db.php';
+if (isset($_SESSION['user_id'])) {
+    $uid = $_SESSION['user_id'];
+    $conn->query("UPDATE users SET remember_token = NULL WHERE id = $uid");
+}
+session_unset();
+session_destroy();
+if (isset($_COOKIE['remember_user'])) {
+    setcookie('remember_user', '', time() - 3600, "/");
+}
 
+header("Location: login.php");
+exit;
+?>
